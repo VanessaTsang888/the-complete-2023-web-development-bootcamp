6455fed45ce4f8be304c497922c24e735284cd87
Vanessa T <tai.v.tsang@gmail.com>
2023-02-24 09:51:26 +0000

    Fixed bugs. Units URL and Spelling error.

--- S20-APIs/246-using-body-parser-to-parse-post/WeatherProject/app.js
+++ S20-APIs/246-using-body-parser-to-parse-post/WeatherProject/app.js
@@ -1,9 +1,4 @@
-/* 
-Snippet as starting point to create a new Node app.
-Make Get Req to external server with Node.js > 
-*/
-
-//jshint esversion:6"
+// jshint esversion:6"
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
@@ -12,43 +7,48 @@ const app = express();
 // Get our app to use the body parser that we required above.
 app.use(bodyParser.urlencoded({extended: true}));
 
-/* The route > home page. 
-Call the GET method on the native HTTPS Node module. Then when we get our results back, we either log the status code or
-simply get hold of the data. Tested my API in UI and passed as have been able to print out the below String and in Terminal
-received code of 200 - ok.
-*/
+/* The route > home page. Call the GET method on the native HTTPS Node module. Then when we get our results back, 
+we either log the status code or simply get hold of the data. */
 app.get("/", function (req, res) {
   // send index.html over to the browser
   res.sendFile(__dirname + "/index.html");
 
 });
-// Catch our root route from the index.html 
-// The cityName is the value from the name prop within the input element within the index.htl
+// Catch our root route from the index.html POST Request - send back certain data we want to printout on the screen.
 app.post("/", function(req, res) {
   const query = req.body.cityName;
   const apiKey = "4b33668b0a04a645d631d93fa31022dc";
-  const unit = "metric";
+  const unit = "metric"; // Convert the unit to metric. 
   // My own API from registeing an account on the Open Weather Map website.
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&=units" + unit;
+    "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
   https.get(url, function (response) {
     console.log(response.statusCode); // code 200 - ok
 
     response.on("data", function (data) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
-      // Get the piece of data we want
+      // Get the piece of data we want from location 0. Arrays are zero based.
       const weatherDescription = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
       // String concatination to add the icon variable.
       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
       // Using template literal to printout the plain text with dynamic data on to the screen. Send the temp and the weather description.
       res.write(
-        `<p>The weather is currently ${weatherDescription}.</p><h1>The tempature in ${query} is ${temp} degrees Celcious.</h1>`
+        `<p>The weather is currently ${weatherDescription}.</p><h1>The tempature in ${query} is ${temp} degrees celsius.</h1>`
       );
-      res.write("It is nice and *HOT* in China.");
+            res.write("<img src=" + imageURL + ">");
+
+      res.write(
+        "<p>I created my own API key from my acc on the OpenWeatherMap website.</p>"
+      );
+      res.write("<small>Then I used my key to develop this weather web app. I used Node.js and the Express framework to develop this app.</small>"
+      );
+      res.write(
+        "<p><small> To run this app locally use this cmd: `nodemon app.js`</small></p>"
+      );
+
       // Send back to the browser an image element that will display the image URL.
-      res.write("<img src=" + imageURL + ">");
       res.send();
     });
   });
@@ -56,7 +56,7 @@ app.post("/", function(req, res) {
 
 
 
-// Print out the below String.
+// Use port 3000 to run this app locally. Print out the below String to test that we are running of this port.
 app.listen(3000, function () {
   console.log("Server is running on port 3000.");
 });
